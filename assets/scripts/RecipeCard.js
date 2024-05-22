8a038e1def6accc110c4bd41425600360e5fd22c
UKGithub <147003715+UKCSD@users.noreply.github.com>
2024-05-21 23:42:33 -0700

    Update RecipeCard.js

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -9,13 +9,13 @@ class RecipeCard extends HTMLElement {
 
 		// EXPOSE - START (All expose numbers start with A)
 		// A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
-		let shadowEl = this.attachShadow({ mode: 'open' });
+		this.attachShadow({ mode: 'open' });
 		// A2. TODO - Create an <article> element - This will hold our markup once our data is set
-		this.article = document.createElement('article'); 
+		let article = document.createElement('article'); 
 		// A3. TODO - Create a style element - This will hold all of the styles for the Web Component
-		this.style = document.createElement('style');
+		let style = document.createElement('style');
 		// A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made (copy everything INSIDE the <style> tag>)
-		this.style.textContent = `
+		style.textContent = `
   		* {
     	font-family: sans-serif;
     	margin: 0;
@@ -94,8 +94,10 @@ class RecipeCard extends HTMLElement {
 		`;
 
 		// A5. TODO - Append the <style> and <article> elements to the Shadow DOM
-		shadowEl.append(this.style);
-		shadowEl.append(this.article);
+
+
+		this.shadowRoot.append(style);
+		this.shadowRoot.append(article);
 	}
 
 	/**
@@ -135,25 +137,19 @@ class RecipeCard extends HTMLElement {
 		//           i.e. imgSrc, titleLnk, etc
 
 		article.innerHTML = `
-		
-		<img src="${data.imgSrc}"
-		alt="${data.imgAlt}">
-	  	<p class="title">
-		<a href="${data.titleLnk}">${data.titleTxt}</a>
-	  	</p>
-	  	<p class="organization">${data.organization}</p>
-	  	<div class="rating">
-		<span>${data.rating}</span>
-		<img src="/lab7-starter/assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">  
-		<span>(${data.numRatings})</span>
-	  	</div>
-	  	<time>${data.lengthTime}</time>
-	  	<p class="ingredients">
-		${data.ingredients}
-	  	</p>
-		
-		
-		`;
+		<img src="${data.imgSrc}" alt="${data.imgAlt}">
+		<p class="title">
+		  <a href="${data.titleLnk}">${data.titleTxt}</a>
+		</p>
+		<p class="organization">${data.organization}</p>
+		<div class="rating">
+		  <span>${data.rating}</span>
+		  <img src="./assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
+		  <span>(${data.numRatings})</span>
+		</div>
+		<time>${data.lengthTime}</time>
+		<p class="ingredients">${data.ingredients}</p>
+	  `;
 	}
 }
 
